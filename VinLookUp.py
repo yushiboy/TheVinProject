cd9c6d9c967cedbf9e86dab586c69b2b5d9d1386
Clayton Robbins <robbinsclay@gmail.com>
2020-05-30 06:56:51 -0400

    I added the instructions to the top of the page.

---
+++ VinLookUp.py
@@ -0,0 +1,5 @@
+#This program must include the following features:
+##Ability to take the vin number from a car and search a database.
+##Ability to take the information that is obtained in step one and display and store as a str()
+##Bonus:
+###Have a search window pop up with videos and documnets pertaining to the car. 
