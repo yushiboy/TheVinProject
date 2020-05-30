56f1d88c73fc804c47f1179814bb36174ae29bdc
Clayton Robbins <robbinsclay@gmail.com>
2020-05-30 07:02:32 -0400

    Added the introduction to the beginning of the program. Added the veriable VIN.

--- VinLookUp.py
+++ VinLookUp.py
@@ -1,5 +1,13 @@
+####Clayton Yushi Robbins####
+
 #This program must include the following features:
 ##Ability to take the vin number from a car and search a database.
 ##Ability to take the information that is obtained in step one and display and store as a str()
 ##Bonus:
-###Have a search window pop up with videos and documnets pertaining to the car. 
+###Have a search window pop up with videos and documnets pertaining to the car.
+###
+
+
+print("Thank you for using my software.")
+print("Please enter your vin number.")
+VIN = input("VIN")
