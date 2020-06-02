01af18cd457b7e245ce3432e6e7e197d7cf227aa
Clayton Robbins <robbinsclay@gmail.com>
2020-06-01 22:30:28 -0400

    Added the steps to have an offline version.

--- VinLookUp.py
+++ VinLookUp.py
@@ -12,6 +12,9 @@ import urllib
 
 def split(vin):
     return [char for char in vin]
+
+##Input the vin and use the split script to make an aray of useable charactors.
+
 print("Thank you for using my software.")
 print("Please enter your vin number.")
 vin = input("Please enter the VIN: ")
@@ -19,3 +22,15 @@ charVIN = []
 charVIN = split(vin)
 print(split(vin))
 print(charVIN)
+
+##The logic aspect of the program
+
+###Takes the first digit and gives the country of origin
+
+###Takes the second digit and gives the manufacturer
+###Takes the thrid digit and tells the vehicle type
+###The fourth through the eight digit are the vehicle discriptor
+###The ninth digit is the "check digit" 
+###The tenth is the year
+###The eleventh is the assembly plant
+###THe twelvth through seventheen is the production number
